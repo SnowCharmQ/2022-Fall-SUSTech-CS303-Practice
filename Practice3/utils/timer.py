42e44b6cf822d22ea4d99294c120d850c10d46eb
SnowCharm <619022098@qq.com>
2022-09-24 16:59:35 +0800

    add supporting python files

---
+++ Practice3/utils/timer.py
@@ -0,0 +1,24 @@
+# 时间控制模块
+import time
+
+
+class TimeController:
+    def __init__(self):
+        self.time_start = 0.0
+        self.time_limit = 0.0
+
+    def start_to_time(self):
+        self.time_start: float = time.time()
+
+    def set_time_limit(self, time_limit):
+        self.time_limit = time_limit
+
+    def have_more_time(self, ratio=0.9, uncount_time=0.1):
+        used = time.time() - self.time_start
+        return used + uncount_time < ratio * self.time_limit
+
+    def get_time_used(self):
+        return time.time() - self.time_start
+
+
+time_controller = TimeController()
